f35626a833b56ff36ae34d67d33a28fa1087409d
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-09 23:24:42 +0500

    N

--- blogyek/urls.py
+++ blogyek/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('accounts/', include('users.urls')),
     path('admin/', admin.site.urls),
     path('darslik/', include('blog.urls')),
-    path('quiz/', include('exam.onlinexam.urls'))
+    path('quiz/', include('exam.onlinexam.urls')),
+    path('studentclick', include('exam.student.urls')),
+    path('teacherclick', include('exam.teacher.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- exam/teacher/urls.py
+++ exam/teacher/urls.py
@@ -3,7 +3,7 @@ from exam.teacher import views
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-path('teacherclick', views.teacherclick_view),
+path('', views.teacherclick_view),
 path('teacherlogin', LoginView.as_view(template_name='teacher/teacherlogin.html'),name='teacherlogin'),
 path('teachersignup', views.teacher_signup_view,name='teachersignup'),
 path('teacher-dashboard', views.teacher_dashboard_view,name='teacher-dashboard'),
